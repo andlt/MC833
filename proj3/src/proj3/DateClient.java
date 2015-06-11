c54620a88eab38a4831947b57fd1d1dba50f87c8
André de Lannoy Tavares <andretavares30@gmail.com>
2015-06-11 17:33:50 -0300

    Medição de tempo e número de repetições na entrada do Cliente

--- proj3/src/proj3/DateClient.java
+++ proj3/src/proj3/DateClient.java
@@ -1,7 +1,9 @@
 package proj3;
 
+import java.io.IOException;
 import java.rmi.Naming;
 import java.util.Date;
+import java.util.Scanner;
 
 /* A very simple client for the RemoteDate service. */
 public class DateClient {
@@ -10,13 +12,27 @@ public class DateClient {
 	protected static RemoteDate netConn = null;
 
 	public static void main(String[] args) {
-		try {
-			netConn = (RemoteDate)Naming.lookup(RemoteDate.LOOKUPNAME);
-			Date today = netConn.getRemoteDate();
-			System.out.println(today.toString());
-		} catch (Exception e) {
-			System.err.println("RemoteDate exception: " + e.getMessage());
-			e.printStackTrace();
+		
+		System.out.println("Número de repetições desejadas:");
+		Scanner scanner = new Scanner(System.in);  
+		int repetitions = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(repetitions);
+		
+		for(int i = 0; i < repetitions; i++) {
+			try {
+				long startTime = System.nanoTime();
+				netConn = (RemoteDate)Naming.lookup(RemoteDate.LOOKUPNAME);
+				Date today = netConn.getRemoteDate();
+				long estimatedTime = System.nanoTime() - startTime;
+				System.out.println(today.toString());
+				System.out.println("Time elapsed: "+estimatedTime/*/1000000*/);
+			} catch (Exception e) {
+				System.err.println("RemoteDate exception: " + e.getMessage());
+				e.printStackTrace();
+			}
 		}
+
 	}
 }
